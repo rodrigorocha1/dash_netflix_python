26c269590896144dd2bbb9093e934eb82eededa7
rodrigorocha1 <silva.rodrigo31@outlook.com>
2022-10-11 23:08:35 -0300

    Configuração layout acesso

--- analise_acessos/dashboard_acesso.py
+++ analise_acessos/dashboard_acesso.py
@@ -21,6 +21,7 @@ layout = html.Div(
                                'top': '301px',
                                'background': '#341A20',
                                'border-radius': '40px'},
+
                         md=4, id='id_assistidas_ano'),
                 dbc.Col(style={'position': 'absolute',
                                'width': '1146px',
@@ -29,6 +30,40 @@ layout = html.Div(
                                'top': '301px',
                                'background': '#341A20',
                                'border-radius': '40px', }, md=4, id='id_assistidas_mes'),
+            ]
+        ),
+        dbc.Row(
+            [
+                dbc.Col(
+                    style={'position': 'absolute',
+                           'width': '577px',
+                           'height': '365px',
+                           'left': '180px',
+                           'top': '679px',
+                           'background': '#341A20',
+                           'border-radius': '40px'},
+                    id='id_tabela_turno'
+                ),
+                dbc.Col(
+                    style={'position': 'absolute',
+                           'width': '577px',
+                           'height': '365px',
+                           'left': '800px',
+                           'top': '679px',
+                           'background': '#341A20',
+                           'border-radius': '40px', }
+                ),
+                dbc.Col(
+                    style={
+                        'position': 'absolute',
+                        'width': '549px',
+                        'height': '365px',
+                        'left': '1400px',
+                        'top': '679px',
+                        'background': '#341A20',
+                        'border-radius': '40px',
+                    }
+                )
             ]
         )
     ]
