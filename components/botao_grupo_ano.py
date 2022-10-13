84f884b6e68a66e7b4d1a712f3c17185df6c6302
rodrigorocha1 <silva.rodrigo31@outlook.com>
2022-10-12 21:03:35 -0300

    correções

--- components/botao_grupo_ano.py
+++ components/botao_grupo_ano.py
@@ -1,16 +1,14 @@
 from dash import html, dcc
 import dash_bootstrap_components as dbc
 
-opcao_periodo = [{'label': 'período completo', 'value': 0}]
+opcao_periodo = [{'label': 'Período completo', 'value': 0}]
 periodo = [i for i in range(2018, 2023)]
 for i, j in zip(periodo, periodo):
     opcao_periodo.append({'label': i, 'value': j})
 
 botao_grupo = dbc.RadioItems(
     id="id_slider_ano",
-    className="btn-group",
-    inputClassName="btn-check",
-    labelClassName="btn btn-outline-primary",
+    inline=True,
     labelCheckedClassName="active",
     options=opcao_periodo,
 )
